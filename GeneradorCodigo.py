a4ad454ba376031b937f0bbb224f143f7755167c
AndresMasis <67446603+AndresMasis@users.noreply.github.com>
2021-03-16 15:08:14 -0600

    GENERATOR Final version for PYTHON
    It creates a series of if else to print the name of roman number in braile.

--- GeneradorCodigo.py
+++ GeneradorCodigo.py
@@ -1,3 +1,70 @@
+# Prints one letter UPPER part in Braile
+def imprimirArriba(cadena):
+    forma1 = 'aehloruvz' # Forma *.
+    forma2 = 'cdmnpy' # Forma **
+    forma3 = 'ist' #Forma .*
+
+    resultado = ""
+    for letra in cadena:
+        if letra == ' ':
+            resultado += "  "
+        if letra in forma1:
+            resultado += "*. "
+        if letra in forma2:
+            resultado += "** "
+        if letra in forma3:
+            resultado += ".* "
+
+    return resultado
+
+
+# Prints one letter MIDDLE part in Braile
+def imprimirMitad(cadena):
+    forma1 = 'ilsv' # Forma *.
+    forma2 = 'hqrt' # Forma **
+    forma3 = 'denoyz' # Forma .*
+    forma4 = 'acmu' # Forma ..
+
+    resultado = ""
+    for letra in cadena:
+        if letra == ' ':
+            resultado += "  "
+        if letra in forma1:
+            resultado += "*. "
+        if letra in forma2:
+            resultado += "** "
+        if letra in forma3:
+            resultado += ".* "
+        if letra in forma4:
+            resultado += ".. "
+
+    return resultado
+
+
+# Prints one letter LOWER part in Braile
+def imprimirAbajo(cadena):
+    forma1 = 'lmnoqrst' # Forma *.
+    forma2 = 'uvyz' # Forma **
+    forma3 = 'acdehi' # Forma ..
+
+
+    resultado = ""
+    for letra in cadena:
+        if letra == ' ':
+            resultado += "  "
+        if letra in forma1:
+            resultado += "*. "
+        if letra in forma2:
+            resultado += "** "
+        if letra in forma3:
+            resultado += ".. "
+
+
+    return resultado
+
+
+
+
 # Uploads the list of roman numbers
 file = open('romanos.txt', 'r')
 listaRomanosFea = file.readlines()
@@ -19,11 +86,17 @@ listaNumerosPal = []
 for e in listaRomanosFea:
     listaNumerosPal.append(e.strip())
 
+print(listaRomanos)
+print(listaNumerosPal)
+
 
 #Creates the code of if, according to wich roman numeral is created, what number in words must return
+
 i = 0
 for roman in listaRomanos:
     print("if entrada == '" + roman + "':")
-    print(" print('" + listaNumerosPal[i] + "')")
+    print(" print('" + imprimirArriba(listaNumerosPal[i]) + "')")
+    print(" print('" + imprimirMitad(listaNumerosPal[i]) + "')")
+    print(" print('" + imprimirAbajo(listaNumerosPal[i]) + "')")
     print("")
     i += 1
